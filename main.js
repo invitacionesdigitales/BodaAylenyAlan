e46d1910cff2662c8cca7a86ebf4f7c6f12d6914
invitacionesdigitales <111189010+invitacionesdigitales@users.noreply.github.com>
2024-07-19 09:11:17 -0300

    dos

--- main.js
+++ main.js
@@ -69,7 +69,7 @@ boton.addEventListener("click", () => {
       icon: 'success',
       html:
         'INGRESÁ' +
-        '' +
+        '<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13085.906183461464!2d-57.9292108!3d-34.9195836!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a2e61ce7c998a7%3A0x57bd083f7fde9725!2sAzul%20Multieventos!5e0!3m2!1ses-419!2sar!4v1721390975598!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
         'Podes ampliar el mapa',
         
         
